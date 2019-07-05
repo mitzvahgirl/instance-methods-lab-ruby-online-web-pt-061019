82dedc676b25a13e3c49e89af6b1bb72a42d1376
Amy Fogelman <abfnj1@gmail.com>
2019-07-05 20:08:57 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  end  
+  
+def talk
+  puts "Hello World!" 
+     ada = Person.new
+     ada.talk
+     talk
+ end  
+ end
+ 
+def walk
+  puts "The Person is walking"
+  ada = Person.new
+  ada.walk
+  walk
+end  
+end
