6c75223a16fbdd3039c2ff1de07729fb616388ca
Amy Fogelman <abfnj1@gmail.com>
2019-07-05 20:28:57 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+ end
+  
+def bark
+    puts "Woof!"
+  end 
+fido = Dog.new
+fido.bark 
+bark
+
+  
+def sit
+    puts "The Dog is sitting"
+  end
+
+ fido = Dog.new
+ fido.sit
+ sit
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  end  
+  
+def talk
+  puts "Hello World!" 
+end
+  ada = Person.new
+  ada.talk
+ talk
+
+ 
+def walk
+  puts "The Person is walking"
+end
+  ada = Person.new
+  ada.walk
+walk
+
